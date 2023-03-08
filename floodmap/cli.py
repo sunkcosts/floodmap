f47f8e4914443c3397ad3c778079b49e9a75870b
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-08 16:31:34 -0500

    floodmap/cli: update

--- floodmap/cli.py
+++ floodmap/cli.py
@@ -1,5 +1,8 @@
 import click
+import requests
+from floodmap.cache import make_cache
 from floodmap.term import vprint, clear
+from floodmap.env import CACHE, ENDPOINT
 
 
 @click.group()
@@ -9,6 +12,20 @@ def entry():
 
 @entry.command()
 def configure():
+    make_cache()
     clear()
-    vprint("[green]Mapbox API Key[/green]")
+    vprint("[blue]Mapbox API Key[/blue]")
     api_token = str(input("❯ "))
+    resp = requests.get(ENDPOINT.tokens(api_token))
+    code = resp.json()["code"]
+    if code == "TokenValid":
+        with open(f"{CACHE}/mapbox", "w") as token:
+            token.write(api_token)
+        token.close()
+        vprint("[green]Token saved to local cache[\green]")
+    elif code == "TokenMalformed":
+        vprint("[red]API Key Invalid[/red]")
+        exit()
+    else:
+        vprint("[red]Unidentified Error[/red]")
+        exit()
