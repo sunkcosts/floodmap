5eadc3b5d5cef816c541785847a2ed28bbf9c283
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-08 16:08:02 -0500

    setup: create

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name="floodmap",
+    version="0.0.2",
+    author="Hart Traveller",
+    url="https://github.com/sunkcosts/floodmap",
+    license="MIT",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["click", "requests"],
+)
