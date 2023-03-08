e6b63f11f01c1aa42b54fee45fe8a755e1634df7
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-08 16:12:27 -0500

    floodmap/term: update

--- floodmap/term.py
+++ floodmap/term.py
@@ -1,3 +1,4 @@
+import os
 from rich.console import Console
 from typing import Any
 
@@ -5,3 +6,7 @@ from typing import Any
 def vprint(x: Any, verbose: bool = True) -> None:
     if verbose:
         Console().print(x)
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
